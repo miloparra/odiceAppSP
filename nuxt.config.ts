f37d3351a9513dde19f1708a24f24a26fbcf88bb
miloparra <miloparra1531.mp@gmail.com>
2026-09-25 10:33:37 +0200

    App - Fixe the vite-plugin-inspect issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,7 +63,7 @@ export default defineNuxtConfig({
     }
   },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   vite: {
     plugins: [tailwindcss()],
   },
